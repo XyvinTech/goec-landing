425bb767c8b52663ff08c3ca225c63f1886ef1c0
anjanavu <anjanavu2000@gmail.com>
2024-10-31 11:59:14 +0530

    layout

---
+++ src/app/stations/page.jsx
@@ -0,0 +1,12 @@
+import AdminLayout from '@/components/admin/AdminLayout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <AdminLayout>
+      Halo
+    </AdminLayout>
+  )
+}
+
+export default page
---
+++ src/components/admin/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const Header = ({ toggleSidebar, isSidebarCollapsed }) => {
+  return (
+    <div className="bg-white p-4 px-6 flex items-center justify-between">
+      <motion.div
+        onClick={toggleSidebar}
+        animate={{ rotate: isSidebarCollapsed ? 180 : 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <Image
+          src="/svg/left.svg"
+          width={18}
+          height={18}
+          alt="Toggle Sidebar"
+          className="cursor-pointer"
+        />
+      </motion.div>
+      <div className="flex items-center">
+        <img src="https://github.com/shadcn.png" alt="Avatar" className="w-8 h-8 rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
